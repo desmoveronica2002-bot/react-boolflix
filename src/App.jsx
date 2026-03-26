ff7751e1401414be5a5e424559bbe11a9c955a74
Veronica Golinelli <desmoveronica2002@gmail.com>
2026-03-26 17:18:33 +0100

    Milestone 1

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,52 @@
-import React, { useState } from 'react'
+import { useState } from 'react'
+
 
 
 function App() {
+  const apiKey = import.meta.env.VITE_API_KEY
 
-const api_key = import.meta.env.VITE_API_KEY
-console.log(api_key)
-const url = `https://api.themoviedb.org/3/search/movie?api_key=${api_key}&query=ritorno+al+futuro`
-console.log(url)
+  const [cerca, setCerca] = useState('') //testo che l’utente scrive nella barra di ricerca
+  const [film, setFilm] = useState([]) // array risultati ricerca film
 
+  const cercaFilm = () => {
 
+    const url = `https://api.themoviedb.org/3/search/movie?api_key=${apiKey}&query=${cerca}`
+
+    fetch(url)
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data)
+        setFilm(data.results) //salvo i risultati 
+      })
+  }
 
   return (
-    <></>
+    <div className="container">
+      <h1>Boolflix</h1>
+
+      <div>
+        <input
+          type="text"
+          value={cerca}
+          onChange={(event) => setCerca(event.target.value)}
+          onKeyDown={(event) => event.key === 'Enter' && cercaFilm()}
+        />
+
+        <button onClick={cercaFilm}>Cerca</button>
+      </div>
+
+
+      <div>
+        {film.map((film) => (
+          <div key={film.id}>
+            <h3>{film.title}</h3>
+            <p><strong>Titolo originale:</strong> {film.original_title}</p>
+            <p><strong>Lingua:</strong> {film.original_language}</p>
+            <p><strong>Voto:</strong> {film.vote_average}</p>
+          </div>
+        ))}
+      </div>
+    </div>
   )
 }
 
