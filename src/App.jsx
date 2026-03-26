242c4c9aae2965c11fc534c95ecb7bf0e1608a01
Veronica Golinelli <desmoveronica2002@gmail.com>
2026-03-26 16:27:44 +0100

    api

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,17 @@
+import React, { useState } from 'react'
 
 
 function App() {
-  
+
+const api_key = import.meta.env.VITE_API_KEY
+console.log(api_key)
+const url = `https://api.themoviedb.org/3/search/movie?api_key=${api_key}&query=ritorno+al+futuro`
+console.log(url)
+
+
 
   return (
-    <>
-
-    </>
+    <></>
   )
 }
 
