229b8215bfec2d70db80ca19d607a47e22b0e649
Veronica Golinelli <desmoveronica2002@gmail.com>
2026-03-27 17:14:32 +0100

    finish milleston 4

--- src/components/AppMain.jsx
+++ src/components/AppMain.jsx
@@ -6,15 +6,16 @@ export default function AppMain ({film, serie, linguaBandiera, generaStelle}){
       <div className="row g-4">
         {film.map((film) => (
           <div key={film.id} className="col-12 col-md-6 col-lg-4">
-            <div className="card h-100 shadow-sm">
+            <div className="card h-100 shadow-sm position-relative overflow-hidden">
               <img src={`https://image.tmdb.org/t/p/w342${film.poster_path}`}className="card-img-top" />
 
-                <div className="card-body d-flex flex-column">
-                  <h5 className="card-title">{film.title}</h5>
-                  <p className="card-text mb-2"><strong>Titolo originale:</strong>{film.original_title}</p>
-                  <p className="card-text mb-2"><strong>Lingua:</strong>{linguaBandiera(film.original_language)}</p>
-                  <p className="card-text mt-auto"><strong>Voto:</strong>{generaStelle(film.vote_average)}</p>
-                </div>
+              <div className="position-absolute w-100 h-100 d-flex flex-column justify-content-center overlay">
+                <h5 className="card-title text-white p-1">{film.title}</h5>
+                <p className="card-text text-white p-3"><strong>Titolo originale:</strong> {film.original_title}</p>
+                <p className="card-text text-white p-3"><strong>Lingua:</strong> {linguaBandiera(film.original_language)}</p>
+                <p className="card-text text-white p-3"><strong>Voto:</strong> {generaStelle(film.vote_average)}</p>
+                <p className="card-text text-white p-3">{film.overview}</p>
+              </div>
 
             </div>
           </div>
