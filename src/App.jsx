29aa888d48ea0bd9eaa44c0dfcf7496b26339376
Veronica Golinelli <desmoveronica2002@gmail.com>
2026-03-27 11:29:48 +0100

    start milleston 3

--- src/App.jsx
+++ src/App.jsx
@@ -97,6 +97,28 @@ const linguaBandiera = (lingua) => {
         {film.map((film) => (
           <div key={film.id}>
             <h3>{film.title}</h3>
+
+             {/*
+             - Aggiungiamo la copertina del film o della serie-
+            
+             Ci viene passata dall API solo la parte finale dell URL
+             
+             Prendere quindi l URL base delle immagini di TMDB: https://image.tmdb.org/t/p/ per poi aggiungere la dimensione che vogliamo generare
+
+             Poi aggiungere la parte finale dell URL passata dall API
+ 
+             */}
+
+
+
+
+
+
+
+
+
+
+
             <p><strong>Titolo originale:</strong> {film.original_title}</p>
             <p><strong>Lingua:</strong> {linguaBandiera(film.original_language)}</p>
             <p><strong>Voto:</strong> {film.vote_average}</p>
@@ -108,3 +130,4 @@ const linguaBandiera = (lingua) => {
 }
 
 export default App
+
