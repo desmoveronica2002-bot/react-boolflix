82ce230515d5474e5d89afe8ff967e2140b25045
Veronica Golinelli <desmoveronica2002@gmail.com>
2026-03-27 16:20:47 +0100

    start boostrap

--- src/components/AppMain.jsx
+++ src/components/AppMain.jsx
@@ -2,29 +2,24 @@ export default function AppMain ({film, serie, linguaBandiera, generaStelle}){
 
 
   return (
-    <main>
+    <main className="my-4">
+      <div className="row g-4">
         {film.map((film) => (
-            <div key={film.id}>
-               <h3>{film.title}</h3>
+          <div key={film.id} className="col-12 col-md-6 col-lg-4">
+            <div className="card h-100 shadow-sm">
+              <img src={`https://image.tmdb.org/t/p/w342${film.poster_path}`}className="card-img-top" />
 
-                {/*
-               - Aggiungiamo la copertina del film o della serie-
-            
-                Ci viene passata dall API solo la parte finale dell URL
-                Prendere quindi l URL base delle immagini di TMDB: https://image.tmdb.org/t/p/ per poi aggiungere la dimensione che vogliamo generare
-                Poi aggiungere la parte finale dell URL passata dall API
-                */}
-
-               <img src={`https://image.tmdb.org/t/p/w342${film.poster_path }`}/>
-
-
-
-               <p><strong>Titolo originale:</strong> {film.original_title}</p>
-               <p><strong>Lingua:</strong> {linguaBandiera(film.original_language)}</p>
-               <p><strong>Voto:</strong>{generaStelle(film.vote_average)}</p>
+                <div className="card-body d-flex flex-column">
+                  <h5 className="card-title">{film.title}</h5>
+                  <p className="card-text mb-2"><strong>Titolo originale:</strong>{film.original_title}</p>
+                  <p className="card-text mb-2"><strong>Lingua:</strong>{linguaBandiera(film.original_language)}</p>
+                  <p className="card-text mt-auto"><strong>Voto:</strong>{generaStelle(film.vote_average)}</p>
+                </div>
 
             </div>
+          </div>
         ))}
+      </div>
     </main>
   );
 }
